fb90bd2a6f2c7665bf2b314388c14eaec97a6a73
Manjunath <girisha.manjunath@capgemini.com>
2019-02-06 14:42:13 +0530

    edited

---
+++ NotificationEvent.java
@@ -0,0 +1,43 @@
+package com.ledify.notification.event.notification_event_publisher;
+
+public class NotificationEvent {
+	
+	private String transType;
+	private String sagreementNo;
+	private String eventType;
+	private String eventDtl;
+	private String eventDt;
+	public String getTransType() {
+		return transType;
+	}
+	public void setTransType(String transType) {
+		this.transType = transType;
+	}
+	public String getSagreementNo() {
+		return sagreementNo;
+	}
+	public void setSagreementNo(String sagreementNo) {
+		this.sagreementNo = sagreementNo;
+	}
+	public String getEventType() {
+		return eventType;
+	}
+	public void setEventType(String eventType) {
+		this.eventType = eventType;
+	}
+	public String getEventDtl() {
+		return eventDtl;
+	}
+	public void setEventDtl(String eventDtl) {
+		this.eventDtl = eventDtl;
+	}
+	public String getEventDt() {
+		return eventDt;
+	}
+	public void setEventDt(String eventDt) {
+		this.eventDt = eventDt;
+	}
+	
+		
+
+}
